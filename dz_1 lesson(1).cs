856d4d0442a5e29b814e623fbc6dcebe904bc5bb
Karter25 <59730654+Karter25@users.noreply.github.com>
2020-01-12 21:07:44 +0600

    Add files via upload

---
+++ dz_1 lesson(1).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите 3-ч значное число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cотни:" + a / 100);
+            int b = Convert.ToInt32(a / 100);
+            Console.WriteLine("десятки:" + (b * 100 - a) / 10 * -1);
+            int c = Convert.ToInt32((b * 100 - a) / 10 * -1);
+            Console.WriteLine("единицы:" + (a - b * 100 - c * 10));
+            Console.ReadKey();
+        }
+    }
+}
